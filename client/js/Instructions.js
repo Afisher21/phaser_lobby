3a87b593fca4f5705d0b04d36cf32c251d226d33
Austin Fisher <austindfisher2006@yahoo.com>
2016-04-06 03:01:30 +0000

    Moved stuff on Instructions for improvement

--- client/js/Instructions.js
+++ client/js/Instructions.js
@@ -6,9 +6,9 @@ pRace.Instructions.prototype = {
         
     
         this.game.add.text(0,90,'Controls: arrow keys to move\n "r" to reset yourself\n "t" to activate traps' , {font: '14px Arial', fill: '#fff'});
-        this.game.add.text(0,130,'Collect the powerstar to gain the ability to double jump. Good luck!' , {font: '14px Arial', fill: '#fff'});
+        this.game.add.text(0,190,'Collect the powerstar to gain the ability to double jump. Good luck!' , {font: '14px Arial', fill: '#fff'});
     
-        button_back = this.game.add.button(160, 500, 'BackImg', this.backButton, this);
+        button_back = this.game.add.button(0, 20, 'BackImg', this.backButton, this);
     },
     update: function(){
         if( pRace_frames === 1){
